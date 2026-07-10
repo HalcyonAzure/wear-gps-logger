ab8de82c6a62f0a811bdaf4a7b27cb8d28efa1c2
HalcyonAzure <53591299+HalcyonAzure@users.noreply.github.com>
2026-07-11 01:34:02 +0800

    fix: remove conflicting allprojects repositories block
    settings.gradle.kts uses RepositoriesMode.FAIL_ON_PROJECT_REPOS,
    which conflicts with allprojects { repositories } in build.gradle.kts.
    Repositories are already defined in dependencyResolutionManagement.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,5 @@ plugins {
     id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+// Repositories are defined in settings.gradle.kts via dependencyResolutionManagement
+// Do NOT add allprojects { repositories } here - it conflicts with RepositoriesMode.FAIL_ON_PROJECT_REPOS
